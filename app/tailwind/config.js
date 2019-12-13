10fb5b2cfdb4f5495c9a103e5be2d6e610e6937f
Benjamin Rosas <ben@aliencyb.org>
2019-12-13 00:05:35 -0600

    main layout elements and style and fonts and stuff

--- app/tailwind/config.js
+++ app/tailwind/config.js
@@ -2,7 +2,30 @@
 
 module.exports = {
   theme: {
-    extend: {}
+    extend: {},
+    fontFamily: {
+      // Tailwind defaults:
+      sans: [
+        '-apple-system',
+        'BlinkMacSystemFont',
+        'Segoe UI',
+        'Roboto',
+        'Helvetica Neue',
+        'Arial',
+        'Noto Sans',
+        'sans-serif',
+        'Apple Color Emoji',
+        'Segoe UI Emoji',
+        'Segoe UI Symbol',
+        'Noto Color Emoji'
+      ],
+      serif: 'Georgia, Cambria, Times New Roman, Times, serif',
+      // mono: 'Menlo, Monaco, Consolas, Liberation Mono, Courier New, monospace',
+
+      // My fonts
+      title: 'rig-solid-bold-fill, sans-serif',
+      mono: 'covik-sans-mono, sans-serif'
+    }
   },
   variants: {},
   plugins: []
